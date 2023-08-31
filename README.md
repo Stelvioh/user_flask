8fa8a1135ebaffa8950b4de865b0c9f96f8512c0
stelvio <stelvioh@gmail.com>
2023-08-31 16:39:23 -0300

    Tentativa com erro e conflito:  ImportError: cannot import name 'parse_rule' from 'werkzeug.routing'

---
+++ README.md
@@ -0,0 +1,8 @@
+# Flask - sqlite  ESEG - 
+
+Terminal:
+
+conda create --name flask_sqlite
+conda activate flask_sqlite
+conda install pip
+pip install flask Flask-SQLAlchemy Flask-JWT-Extended marshmallow Flask-Classful sqlalchemy-filters
--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,19 +1,16 @@
-from flask import Blueprint, request, jsonify
+from flask_classful import FlaskView, route
+from service.user_service import UserService
+from flask import request, jsonify
 
-class UserController:
-    blueprint = Blueprint('users', __name__)
+class UserController(FlaskView):
 
-    @staticmethod
-    @blueprint.route('/', methods=['GET'])
-    def get_all_users():
-        from service.user_service import UserService
+    @route('/', methods=['GET'])
+    def get_all_users(self):
         userDtoLists = UserService.get_all_users()
         return jsonify([user_dto.__dict__ for user_dto in userDtoLists])
 
-    @staticmethod
-    @blueprint.route('/', methods=['POST'])
-    def add_user():
-        from service.user_service import UserService
+    @route('/', methods=['POST'])
+    def add_user(self):
         user_data = request.get_json()
         userDto = UserService.add_user(user_data)
         return jsonify(userDto.__dict__), 201
