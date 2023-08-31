57ce9b4bef71f50223f3bcbe0b088e055d0221dc
stelvio <stelvioh@gmail.com>
2023-08-31 12:51:34 -0300

    Ajuste na ordem dos métodos UserService

--- service/user_service.py
+++ service/user_service.py
@@ -2,12 +2,12 @@ from repository.user_repository import UserRepository
 from domain.user import User
 
 class UserService:
+
+    @staticmethod
+    def get_all_users():
+        return UserRepository.get_all_users()
     
     @staticmethod
     def add_user(user_dto):
         user = User(name=user_dto['name'], email=user_dto['email'])
         return UserRepository.add_user(user)
-
-    @staticmethod
-    def get_all_users():
-        return UserRepository.get_all_users()
