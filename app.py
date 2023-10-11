e839bdd68e47db855e331be853c3ca27a275b013
stelvio <stelvioh@gmail.com>
2023-10-11 17:00:18 -0300

    Permiter acesso a todas as requisições

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
+from flask_cors import CORS  # Já importado, apenas destacando.
 from domain.user import User
 from database import db
 from config import Config
 import controller.user_controller as controller_user
 
-
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    # Aplicando CORS para a aplicação. 
+    # Isto permitirá que todas as origens acessem sua API. 
+    CORS(app)
 
     db.init_app(app)
     
@@ -17,8 +21,6 @@ def create_app():
         db.create_all()    
 
     return app
-
-
 
 if __name__ == '__main__':
     app = create_app()
